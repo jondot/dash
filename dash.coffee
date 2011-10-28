8687e4dda93dfc3e0a2cd2059b1e4ce40cb50608
Dotan Nahum <jondotan@gmail.com>
2011-10-28 19:14:50 +0200

    adding xhr for heroku

--- dash.coffee
+++ dash.coffee
@@ -1,14 +1,25 @@
 express = require('express')
+sio = require('socket.io')
+
+# on heroku, use $ heroku config:add FORCE_POLLING=10
+if process.env.FORCE_POLLING
+  sio.configure ()->
+    sio.set("transports", ["xhr-polling"])
+    sio.set("polling duration", process.env.FORCE_POLLING)
+
+
 app = express.createServer()
-io  = require('socket.io').listen(app)
-
-
 app.configure ()->
   app.use express.static(__dirname + '/public')
   app.use express.bodyParser()
 
+
 port = process.env.PORT || 3000
+
+
+
 app.listen(port)
+io  = sio.listen(app)
 
 
 
--- dash.js
+++ dash.js
@@ -1,14 +1,21 @@
 (function() {
-  var app, express, io, port;
+  var app, express, io, port, sio;
   express = require('express');
+  sio = require('socket.io');
+  if (process.env.FORCE_POLLING) {
+    sio.configure(function() {
+      sio.set("transports", ["xhr-polling"]);
+      return sio.set("polling duration", process.env.FORCE_POLLING);
+    });
+  }
   app = express.createServer();
-  io = require('socket.io').listen(app);
   app.configure(function() {
     app.use(express.static(__dirname + '/public'));
     return app.use(express.bodyParser());
   });
   port = process.env.PORT || 3000;
   app.listen(port);
+  io = sio.listen(app);
   app.post('/messages', function(req, res) {
     var content;
     content = req.param('content');
