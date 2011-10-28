8c5203b3529cc86a251fb20e9c706e988053e198
Dotan Nahum <jondotan@gmail.com>
2011-10-28 19:19:19 +0200

    blunder

--- dash.coffee
+++ dash.coffee
@@ -1,25 +1,22 @@
 express = require('express')
-sio = require('socket.io')
+app = express.createServer()
+io  = require('socket.io').listen(app)
 
 # on heroku, use $ heroku config:add FORCE_POLLING=10
 if process.env.FORCE_POLLING
-  sio.configure ()->
-    sio.set("transports", ["xhr-polling"])
-    sio.set("polling duration", process.env.FORCE_POLLING)
+  io.configure ()->
+    io.set("transports", ["xhr-polling"])
+    io.set("polling duration", process.env.FORCE_POLLING)
 
 
-app = express.createServer()
 app.configure ()->
   app.use express.static(__dirname + '/public')
   app.use express.bodyParser()
 
 
 port = process.env.PORT || 3000
-
-
-
 app.listen(port)
-io  = sio.listen(app)
+
 
 
 
--- dash.js
+++ dash.js
@@ -1,21 +1,20 @@
 (function() {
-  var app, express, io, port, sio;
+  var app, express, io, port;
   express = require('express');
-  sio = require('socket.io');
+  app = express.createServer();
+  io = require('socket.io').listen(app);
   if (process.env.FORCE_POLLING) {
-    sio.configure(function() {
-      sio.set("transports", ["xhr-polling"]);
-      return sio.set("polling duration", process.env.FORCE_POLLING);
+    io.configure(function() {
+      io.set("transports", ["xhr-polling"]);
+      return io.set("polling duration", process.env.FORCE_POLLING);
     });
   }
-  app = express.createServer();
   app.configure(function() {
     app.use(express.static(__dirname + '/public'));
     return app.use(express.bodyParser());
   });
   port = process.env.PORT || 3000;
   app.listen(port);
-  io = sio.listen(app);
   app.post('/messages', function(req, res) {
     var content;
     content = req.param('content');
